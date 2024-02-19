aa6a2bfe10f9f9dc55e20df4d49375be9304525a
Eliúde Vemba <sweeteliude@gmail.com>
2024-02-19 05:06:22 +0100

    Add provider scope
    To access providers globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:great_places/src/common/themes/dark_theme.dart';
 import 'package:great_places/src/common/utils/app_routes.dart';
-import 'package:great_places/src/features/home/data/great_place_repository.dart';
-import 'package:great_places/src/features/home/data/local_great_place_repository.dart';
 import 'package:great_places/src/features/home/presentation/place_detail_page.dart';
 import 'package:great_places/src/features/home/presentation/place_form_page.dart';
 import 'package:great_places/src/features/home/presentation/places_list_page.dart';
-import 'package:great_places/src/features/home/presentation/great_place_controller.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await FlutterConfig.loadEnvVariables();
 
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -23,26 +20,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<GreatPlaceRepository>(
-          create: (_) => LocalGreatPlaceRepository(),
-        ),
-        ChangeNotifierProvider<GreatPlaceController>(
-            create: (context) => GreatPlaceController(
-                repository: context.read<GreatPlaceRepository>()
-            ),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: darkTheme(),
-        routes: {
-          AppRoutes.HOME: (context) => const PlacesListPage(),
-          AppRoutes.FORMROUTE: (context) => const PlaceFormPage(),
-          AppRoutes.DETAIL: (context) => const PlaceDetailPage(),
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: darkTheme(),
+      routes: {
+        AppRoutes.HOME: (context) => const PlacesListPage(),
+        AppRoutes.FORMROUTE: (context) => const PlaceFormPage(),
+        AppRoutes.DETAIL: (context) => const PlaceDetailPage(),
+      },
     );
   }
 }
